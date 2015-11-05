799cbeb40b208ede43f498b082ce1411ea97b8fb
Hon Lam <lamh85@gmail.com>
2015-11-04 17:23:07 -0800

    New constructors and instances.

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ console.log('script has loaded');
 
 var leagueCounter = 1;
 
+var Country = function(name) {
+    this.name = name;
+}
+canada = Country("Canada");
+usa = Country("USA")
+
 var League = function(name, countries) {
     this.id = leagueCounter;
     this.name = name;
@@ -9,9 +15,16 @@ var League = function(name, countries) {
 
     leagueCounter ++;
 }
+var nhl = new League("NHL", [canada, usa])
 
 var Team = function(name, city, league) {
     this.name = name;
     this.city = city;
     this.league = league;
+}
+
+var Player = function(name, birthYear, birthCountry) {
+    this.name = name;
+    this.birthYear = birthYear;
+    this.birthCountry = birthCountry;
 }
